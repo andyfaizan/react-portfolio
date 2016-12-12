8a6994db53e35a7027d7b0cb4c7391804cc378bd
Ainuddin Faizan <andyfaizan@gmail.com>
2016-12-13 00:52:25 +0100

    Added typing, velocity js, parallax

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,83 +1,165 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import '../styles/typing.scss';
+import '../styles/parallax-effects.scss';
 import * as BenchmarkActions from '../actions/BenchmarkActions';
-import Project from '../components/Project';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Statistics from '../components/Statistics';
+// import Footer from '../components/Footer';
+// import Header from '../components/Header';
+// import Statistics from '../components/Statistics';
 import { startTime } from '../index';
 
+const VelocityComponent = require('velocity-react/velocity-component');
 /**
  * It is common practice to have a 'Root' container/component require our main App (this one).
  * Again, this is because it serves to wrap the rest of our application with the Provider
  * component to make the Redux store available to the rest of the app.
  */
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSubComponent: true
+    };
+    this.onScroll = this.onScroll.bind(this);
+  }
+
   componentDidMount() {
     const { actions } = this.props;
     actions.updateBenchmark(new Date().getTime() - startTime);
   }
 
+  onScroll() {
+    if (this.state.showSubComponent) {
+      this.setState({ showSubComponent: false });
+    }
+  }
+
   render() {
-    const { projects, benchmark, personalInfo } = this.props;
-    const projectEntries = projects.map((project, index) => (
-      <Project key={index} project={project} />
-    ));
-    // we can use ES6's object destructuring to effectively 'unpack' our props
+    const addScrollListener = (node) => {
+      if (node) {
+        node.addEventListener('scroll', this.onScroll);
+      }
+    };
+
     return (
-      <div className="main-app-container">
-        <Header personalInfo={personalInfo} />
-        <br />
-        <div style={{ 'align-items': 'center' }}>{projectEntries}</div>
-        <Footer personalInfo={personalInfo} />
-        <Statistics benchmark={benchmark} />
+      // <div className="main-app-container">
+      //   <Header personalInfo={personalInfo} />
+      //   <br />
+      //   <Footer personalInfo={personalInfo} />
+      //   <Statistics benchmark={benchmark} />
+      // </div>
+      <div ref={addScrollListener} className="parallax">
+        <div id="group2" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base" />
+          <div className="parallax__layer parallax__layer--back">
+            <VelocityComponent
+              animation={{ opacity: this.state.showSubComponent ? 1 : 0 }}
+              duration={500}
+            >
+              <strong>
+                <h1 className="typed">
+                  Ainuddin Faizan
+                  <span className="typed-span">&nbsp;</span>
+                </h1>
+              </strong>
+            </VelocityComponent>
+          </div>
+        </div>
+        <div id="group3" className="parallax__group">
+          <div className="parallax__layer parallax__layer--fore">
+            <VelocityComponent
+              animation={'scroll'}
+              duration={150}
+              runOnMount
+            >
+              <div className="title">
+                Some text
+              </div>
+            </VelocityComponent>
+          </div>
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+        </div>
+        <div id="group4" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">
+              Background Layer
+            </div>
+          </div>
+        </div>
+        <div id="group5" className="parallax__group">
+          <div className="parallax__layer parallax__layer--fore">
+            <div className="title">
+              Foreground Layer
+            </div>
+          </div>
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+        </div>
+        <div id="group6" className="parallax__group">
+          <div className="parallax__layer parallax__layer--back">
+            <div className="title">
+              Background Layer
+            </div>
+          </div>
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+        </div>
+        <div id="group7" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="title">
+              Base Layer
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 App.propTypes = {
-  projects: PropTypes.array.isRequired,
+  // projects: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
-  benchmark: PropTypes.number.isRequired,
-  personalInfo: PropTypes.object.isRequired
+  // benchmark: PropTypes.number.isRequired,
+  // personalInfo: PropTypes.object.isRequired,
+  // showSubComponent: PropTypes.bool.isRequired,
 };
 
-/**
- * Keep in mind that 'state' isn't the state of local object, but your single
- * state in this Redux application. 'counter' is a property within our store/state
- * object. By mapping it to props, we can pass it to the child component Counter.
- */
 function mapStateToProps(state) {
   return {
     projects: state.projects,
     benchmark: state.benchmark,
-    personalInfo: state.personalInfo
+    personalInfo: state.personalInfo,
+    showSubComponent: 'true',
   };
 }
 
-/**
- * Turns an object whose values are 'action creators' into an object with the same
- * keys but with every action creator wrapped into a 'dispatch' call that we can invoke
- * directly later on. Here we imported the actions specified in 'CounterActions.js' and
- * used the bindActionCreators function Redux provides us.
- *
- * More info: http://redux.js.org/docs/api/bindActionCreators.html
- */
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(BenchmarkActions, dispatch)
   };
 }
-
-/**
- * 'connect' is provided to us by the bindings offered by 'react-redux'. It simply
- * connects a React component to a Redux store. It never modifies the component class
- * that is passed into it, it actually returns a new connected componet class for use.
- *
- * More info: https://github.com/rackt/react-redux
- */
 
 export default connect(
   mapStateToProps,
--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
-import DevTools from './DevTools';
+// import DevTools from './DevTools';
 
 /**
  * Component is exported for conditional usage in Root.js
@@ -19,7 +19,7 @@ module.exports = class Root extends Component {
         <div>
           <App />
           {/* Being the dev version of our Root component, we include DevTools below */}
-          <DevTools />
+          {/* <DevTools /> */}
         </div>
       </Provider>
     );
