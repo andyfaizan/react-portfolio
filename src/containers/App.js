f56fccd3f0ff28fc4169bfef4d7a5d2abde70572
Ainuddin Faizan <andyfaizan@gmail.com>
2016-12-25 00:12:06 +0100

    Fixed welcome animation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,19 @@
 import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
+// import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import '../styles/typing.scss'
 import '../styles/parallax-effects.scss'
-import * as BenchmarkActions from '../actions/BenchmarkActions'
+import '../styles/headroom.scss'
 // import Footer from '../components/Footer';
-// import Header from '../components/Header';
+import Header from '../components/Header'
+import { personalInfoData } from '../data/personalInfo'
 // import Statistics from '../components/Statistics';
-import { startTime } from '../index'
+// import { startTime } from '../index'
 
 const VelocityComponent = require('velocity-react/velocity-component')
+// require('velocity-animate')
+// require('velocity-animate/velocity.ui')
+const Headroom = require('headroom.js/dist/headroom')
 /**
  * It is common practice to have a 'Root' container/component require our main App (this one).
  * Again, this is because it serves to wrap the rest of our application with the Provider
@@ -22,13 +26,13 @@ class App extends Component {
     this.state = {
       showSubComponent: true
     }
-    this.onScroll = this.onScroll.bind(this)
+    // this.onScroll = this.onScroll.bind(this)
   }
 
-  componentDidMount() {
-    const { actions } = this.props
-    actions.updateBenchmark(new Date().getTime() - startTime)
-  }
+  // componentDidMount() {
+  //   const { actions } = this.props
+  //   actions.updateBenchmark(new Date().getTime() - startTime)
+  // }
 
   onScroll() {
     if (this.state.showSubComponent) {
@@ -36,34 +40,99 @@ class App extends Component {
     }
   }
 
+
   render() {
-    const addScrollListener = (node) => {
-      if (node) {
-        node.addEventListener('scroll', this.onScroll)
+    // const addScrollListener = (node) => {
+    //   if (node) {
+    //     node.addEventListener('scroll', this.onScroll)
+    //   }
+    // }
+
+    const headerInit = (headerElement) => {
+      if (headerElement) {
+        console.log(headerElement)
+        console.log(Headroom)
+        const header = new Headroom(headerElement, {
+          tolerance: 5,
+          offset: 205,
+          classes: {
+            initial: 'animated',
+            pinned: 'slideDown',
+            unpinned: 'slideUp'
+          }
+        })
+        header.init()
+      }
+    }
+
+    const buttonInit = (buttonElement) => {
+      if (buttonElement) {
+        const bttHeadroom = new Headroom(buttonElement, {
+          tolerance: 0,
+          offset: 500,
+          classes: {
+            initial: 'slide',
+            pinned: 'slide--reset',
+            unpinned: 'slide--down'
+          }
+        })
+        bttHeadroom.init()
       }
     }
 
     return (
-      // <div className="main-app-container">
-      //   <Header personalInfo={personalInfo} />
-      //   <br />
-      //   <Footer personalInfo={personalInfo} />
-      //   <Statistics benchmark={benchmark} />
-      // </div>
-      <div ref={addScrollListener} className="parallax">
+      <div className="parallax">
+        <div
+          ref={headerInit}
+          style={{
+            headroom: {
+              top: 0,
+              left: 0,
+              right: 0,
+              zIndex: 10,
+            },
+            'headroom--unfixed': {
+              position: 'relative',
+              transform: 'translateY(0)',
+            },
+            'headroom--scrolled': {
+              transition: 'transform 200ms ease-in-out',
+            },
+            'headroom--unpinned': {
+              position: 'fixed',
+              transform: 'translateY(-100%)',
+            },
+            'headroom--pinned': {
+              position: 'fixed',
+              transform: 'translateY(0%)',
+            }
+          }}
+        >
+          <Header personalInfo={personalInfoData} />
+        </div>
         <div id="group2" className="parallax__group">
           <div className="parallax__layer parallax__layer--base" />
           <div className="parallax__layer parallax__layer--back">
             <VelocityComponent
-              animation={{ opacity: this.state.showSubComponent ? 1 : 0 }}
-              duration={500}
+              animation={{ translateY: '-10vh' }}
+              delay={2000}
+              duration={1000}
+              runOnMount
             >
-              <strong>
-                <h1 className="typed">
-                  Ainuddin Faizan
-                  <span className="typed-span">&nbsp;</span>
-                </h1>
-              </strong>
+              <h1 className="typed">
+                Ainuddin Faizan
+                <span className="typed-span">&nbsp;</span>
+              </h1>
+            </VelocityComponent>
+            <VelocityComponent
+              animation={{ opacity: [1, 0] }}
+              delay={3000}
+              duration={2000}
+              runOnMount
+            >
+              <h1 className="welcome">
+                Welcome
+              </h1>
             </VelocityComponent>
           </div>
         </div>
@@ -155,13 +224,13 @@ function mapStateToProps(state) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(BenchmarkActions, dispatch)
-  }
-}
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     actions: bindActionCreators(BenchmarkActions, dispatch)
+//   }
+// }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  // mapDispatchToProps
 )(App)
