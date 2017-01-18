44a9d2d3f7708f7f34ae7d8a66af4b2be4030202
Ainuddin Faizan <andyfaizan@gmail.com>
2017-01-18 01:54:32 +0100

    Fixed timeline components

--- src/components/About.js
+++ src/components/About.js
@@ -37,10 +37,55 @@ class About extends Component {
         <div style={{ height: '30vh' }} />
         <div>
           <div className="row" style={{ 'margin-bottom': '5vh' }}>
-            <LeftTimelineComponent show={this.props.show} />
+            <div className="col-md-6">
+              <LeftTimelineComponent show={this.props.show} />
+            </div>
+            <div
+              className="pull-right col-md-6"
+              style={{
+                height: '30vh',
+                width: '35vw',
+                margin: 20,
+                textAlign: 'justify',
+                right: '10vw',
+              }}
+            >
+              <h4>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h4>
+            </div>
           </div>
           <div className="row" style={{ 'margin-bottom': '5vh' }}>
-            <RightTimelineComponent show={this.props.show} />
+            <div className="col-md-6">
+              <div
+                style={{
+                  height: '30vh',
+                  width: '35vw',
+                  margin: 20,
+                  textAlign: 'justify',
+                  marginLeft: '100',
+                }}
+              >
+                <h4>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </h4>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <RightTimelineComponent show={this.props.show} />
+            </div>
           </div>
           <div className="row" style={{ 'margin-bottom': '5vh' }}>
             <LeftTimelineComponent show={this.props.show} />
--- src/components/RightTimelineComponent.js
+++ src/components/RightTimelineComponent.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import Paper from 'material-ui/Paper'
 
 const VelocityComponent = require('velocity-react/velocity-component')
+const Image = require('../resources/code.jpg')
 
 const propTypes = {
   show: PropTypes.bool,
@@ -15,7 +16,7 @@ const style = {
   // display: 'inline-block',
   // position: 'absolute',
   // float: 'left',
-  right: '100%',
+  right: '10vw',
   opacity: 0
 }
 
@@ -25,9 +26,11 @@ function RightTimelineComponent({ show }) {
       <VelocityComponent
         animation={{ translateX: show ? '-10vw' : 0, opacity: show ? 1 : 0 }}
         duration={1000}
-        delay={200}
+        delay={500}
       >
-        <Paper className="pull-right" style={style} zDepth={1} />
+        <Paper className="pull-right" style={style} zDepth={1} >
+          <img src={Image} height={'100%'} role="presentation" />
+        </Paper>
       </VelocityComponent>
     </div>
   )
